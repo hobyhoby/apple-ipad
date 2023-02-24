2610a605ea6a57a4d6e47e5a7e3ec4c4ddf99ffd
hobyhoby <hoby@nate.com>
2023-02-24 17:33:28 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -226,6 +226,7 @@ navigations.forEach((nav) => {
   mapEl.innerHTML = /* html */ `
   <h3>
     <span class="text">${nav.title}</span>
+    <span class="icon">+</span>
   </h3>
   <ul>
     ${mapList}
@@ -237,3 +238,12 @@ navigations.forEach((nav) => {
 
 const thisYearEl = document.querySelector("span.this-year");
 thisYearEl.textContent = new Date().getFullYear();
+
+const mapEls = document.querySelectorAll("footer .navigations .map");
+
+mapEls.forEach((el) => {
+  const h3El = el.querySelector("h3");
+  h3El.addEventListener("click", () => {
+    el.classList.toggle("active");
+  });
+});
